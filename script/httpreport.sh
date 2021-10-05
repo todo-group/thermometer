dd0f40abee942944e6562dd12f895b583a00687a
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-10-05 16:51:14 +0900

    httpreport.sh

--- script/httpreport.sh
+++ script/httpreport.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 PROG="$1"
-if [ -z ${PROG} ]; then
-  echo "Error: ${0} path_to_tool"
+SERVER="$2"
+RANDKEY="$3"
+if [ -z ${RANDKEY} ]; then
+  echo "Error: ${0} path_to_tool server randkey"
   exit 127
 fi
 
 RES=$(${PROG} 2> /dev/null | grep temperature | cut -d ' ' -f 4)
-if [ "${RES}" = "" ]; then
+if [ "${RES}" = "" ]; then 
   RES=$(${PROG} 2> /dev/null | cut -d ' ' -f 2 | cut -d : -f 2)
   if [ "${RES}" = "" ]; then
     exit 127
   fi
 fi
 
-echo ${RES}
+curl http://${SERVER}/${RANDKEY}/${RES} > /dev/null 2>&1
